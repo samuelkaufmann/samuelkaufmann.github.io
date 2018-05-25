8e56339bffd27da73c0dd7577beb1e72bea45ade
Samuel Kaufmann <samuel@smartfactory.ch>
2018-05-25 11:53:12 +0200

    update webpage

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
-console.log("READY");
-var currentCommand;
+var selectedCommand;
 var consoleContent;
 
 
 
 function executeCommand(event) {
-	currentCommand = this.options[this.selectedIndex].text;
-	console.log(currentCommand);
+	selectedCommand = this.options[this.selectedIndex].text;
+	console.log(selectedCommand);
+	if (selectedCommand == 'clear') {
+		requestUrl = 'https://maker.ifttt.com/trigger/test_push_notification/with/key/8YTrZWXGvX8AI0abunwjdjDARiLSL1NxGwZbvyXWGZ';
+		makeGetRequest(requestUrl);
+	}
 }
 
 function debug() {
 	alert("DEBUGGING");
 }
 
-function makeRequest(methode, requestUrl) {
+function makeGetRequest(requestUrl) {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState === 4) {
 			var response = JSON.parse(xhr.responseText);
 			console.log(response);
 		}
-		xhr.open(methode, requestUrl);
+		xhr.open('GET', requestUrl);
 		xhr.send();
 	}
 }
